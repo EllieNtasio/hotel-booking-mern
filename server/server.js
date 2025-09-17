729c553c6628f4e18c6e7950f182807eee04cab9
EllieNtasio <ellintasio@gmail.com>
2025-09-17 16:55:06 +0300

    api for backend

--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/ClerkWebhooks.js";
 
 connectDB()
 
 
 const app = express()
 app.use(cors())
+
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.use("/api/clerk", clerkWebhooks);
+
 
 app.get('/', (req, res)=> res.send("API is working") )
 
