1ad5b9065e8951b37d7d5ae26f8408f2f504396c
EllieNtasio <ellintasio@gmail.com>
2025-11-12 20:28:40 +0200

    finished functions for rooms controller, finished API for Hotels

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -47,10 +47,24 @@ export const getRooms = async(req, res)=>{
 }
 
 export const getOwnerRooms = async(req, res)=>{
-
+   try {
+    const hotelData = await Hotel({owner: req.auth.userId})
+    const rooms = await Room.find({hotel: hotelData._id.toString()}).populate("hotel");
+    res.json({success: true, rooms});
+   } catch (error) {
+    res.json({success: false, message: error.message});
+   }
 }
 
 
 export const toggleRoomAvailability = async(req, res)=>{
-
+    try {
+       const {roomId} = req.body;
+       const roomData = await Room.findById(roomId);
+       roomData.isAvailable = !roomData.isAvailable;
+       await roomData.save();
+       res.json({success: true, message: "Room Availability Updated!"});
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
 }
